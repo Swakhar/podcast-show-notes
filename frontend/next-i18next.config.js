73d12753d1f1f30551c9f0fa3d09cac0dd0d0772
swakhar <swakhar.me@gmail.com>
2025-09-19 11:31:00 +0200

    added i18n configuration

---
+++ frontend/next-i18next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
+    localeDetection: true,
+  },
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
+}
